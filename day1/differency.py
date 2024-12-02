39b2d9f46288dd46bfefeb51cadb2bad88524916
Ekuspreet <ekuspreetsingh2@gmail.com>
2024-12-02 08:57:23 +0530

    Day1Problem1

---
+++ day1/differency.py
@@ -0,0 +1,19 @@
+import heapq
+
+left = []
+right = []
+
+with open("input.txt", "r") as file:
+    for line in file:
+        leftnum = int(line.strip().split(" ")[0])
+        rightnum = int(line.strip().split(" ")[-1])
+        heapq.heappush(left, leftnum)
+        heapq.heappush(right, rightnum)
+
+result = 0
+
+for i in range(1000):
+    l = heapq.heappop(left)
+    r = heapq.heappop(right)
+    result = result + ( abs( r - l )  ) 
+print(result)
